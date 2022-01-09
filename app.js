6adbd4dd2d5b74522e893519990d6b97b76b97e5
EddieSpaghetti666 <Dsmith141996@gmail.com>
2022-01-08 19:36:23 -0500

    Implement add/remove UI

--- app.js
+++ app.js
@@ -34,9 +34,59 @@ class UI {
          <td>${book.title}</td>
          <td>${book.author}</td>
          <td>${book.isbn}</td>
-         <td><a href="#" class="btn btn-danger btn-sm delete>X</a></td>
+         <td><a href="#" class="btn btn-danger
+         delete">X</a></td>
         `;
 
     list.appendChild(row);
   }
+
+  static showAlert(message, className) {
+    const div = document.createElement("div");
+    div.className = `alert alert-${className}`;
+    div.appendChild(document.createTextNode(message));
+    const container = document.querySelector(".container");
+    const form = document.querySelector("#book-form");
+    container.insertBefore(div, form);
+
+    setTimeout(() => document.querySelector(".alert").remove(), 2000);
+  }
+
+  static clearFields() {
+    document.querySelector("#title").value = "";
+    document.querySelector("#author").value = "";
+    document.querySelector("#isbn").value = "";
+  }
+
+  static deletebook(elem) {
+    if (!elem.classList.contains("delete")) return;
+    elem.parentElement.parentElement.remove();
+  }
 }
+
+document.addEventListener("DOMContentLoaded", UI.displayBooks);
+
+document.querySelector("#book-form").addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const title = document.querySelector("#title").value;
+  const author = document.querySelector("#author").value;
+  const isbn = document.querySelector("#isbn").value;
+  const book = new Book(title, author, isbn);
+
+  if (title === "" || author === "" || isbn === "") {
+    UI.showAlert("Please fill in all fields", "danger");
+    return;
+  }
+
+  UI.addBookToList(book);
+  UI.showAlert('Book Added', 'success');
+  UI.clearFields();
+});
+
+document
+  .querySelector("#book-list")
+  .addEventListener("click", (e) => {
+      UI.deletebook(e.target);
+      UI.showAlert('Book Removed', 'info');
+  });
